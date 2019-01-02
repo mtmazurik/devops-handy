b07e5380229bfc58070cdc2008e887ab4fcd9864
marty <marty@martymazurik.com>
2019-01-02 11:42:41 -0800

    delete ALL images in ACR repo

--- ACR-delete-all-images.sh
+++ ACR-delete-all-images.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# WARNING! This script deletes data!
-# Run only if you do not have systems
-# that pull images via manifest digest.
+# WARNING! This script deletes data!  All images in ACR Repository will be deleted!
 
 # Change to 'true' to enable image delete
 ENABLE_DELETE=true
@@ -14,7 +12,7 @@ REPOSITORY=lognook
 # Delete all untagged (orphaned) images
 if [ "$ENABLE_DELETE" = true ]
 then
-    az acr repository show-manifests --name $REGISTRY --repository $REPOSITORY  --query "[?tags==null].digest" -o tsv \
+    az acr repository show-manifests --name $REGISTRY --repository $REPOSITORY  -o tsv \
     | xargs -I% az acr repository delete --name $REGISTRY --image $REPOSITORY@% --yes
 else
     echo "No data deleted. Set ENABLE_DELETE=true to enable image deletion."
